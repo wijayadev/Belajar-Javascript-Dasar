63ce7fe7a854501b3e9e9beeec9c51f6641eb565
Wijaya <wijaya5521@gmail.com>
2024-05-26 08:04:05 +0700

    merubah nama folder agar berurutan

--- 3. if else if/angkot2.js
+++
@@ -1,8 +0,0 @@
-// for
-for (let i = 1; i <= 10; i++) {
-    if(i<=7) {
-        console.log('Angkot no ' + i + ' beroperasi dengan baik');
-    } else {
-        console.log('Angkot no ' + i + ' tidak beroperasi');
-    }
-}
